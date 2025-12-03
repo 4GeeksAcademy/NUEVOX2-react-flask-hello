1504a0a80431f03773bb8d8a28f38413146bcba4
Daniela González <danimge94@gmail.com>
2025-12-03 23:12:15 +0000

    todo listo

--- src/api/forms.py
+++ src/api/forms.py
@@ -1,30 +1,49 @@
-from flask import Flask
-from flask_admin import Admin
-from flask import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[
-                           DataRequired(), Length(min=3, max=25)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[
-                             DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[
-                                     DataRequired(), EqualTo('password')])
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=3, max=25)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=6)]
+    )
+    confirm_password = PasswordField(
+        'Confirm Password',
+        validators=[DataRequired(), EqualTo('password')]
+    )
     submit = SubmitField('Register')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
     submit = SubmitField('Login')
 
 
 class PokemonForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    type = StringField('Type', validators=[DataRequired()])
+    name = StringField(
+        'Name',
+        validators=[DataRequired()]
+    )
+    type = StringField(
+        'Type',
+        validators=[DataRequired()]
+    )
     description = TextAreaField('Description')
     image_url = StringField('Image URL')
     submit = SubmitField('Save')
