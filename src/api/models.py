4416d3944a23441e55fd4f822d2cd38960a7cbf9
Daniela González <danimge94@gmail.com>
2025-12-03 16:28:14 +0000

    modelos listos

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_passwor_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, ForeingKey, Text, DateTime, Integer
@@ -8,14 +8,17 @@ from sqlalchemy import String, ForeingKey, Text, DateTime, Integer
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(120), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow)
 
-    favorites: Mapped[list["Favorite"]] = relationship(back_populates='user', cascade='all, delete-orphan')
-
+    favorites: Mapped[list["Favorite"]] = relationship(
+        back_populates='user', cascade='all, delete-orphan')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -25,4 +28,18 @@ class User(db.Model):
 
 
 class Pokemon(db.Model):
-    id: Mapped[int]
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(80), nullable=False)
+    number: Mapped[str] = mapped_column(Integer, nullable=True)
+    sprite: Mapped[str] = mapped_column(String(225), nullable=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+
+
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeingKey('user.id'))
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow)
+
+    user: Mapped["User"] = relationship(back_populates='favorites')
+    pokemon: Mapped["Pokemon"] = relationship()
